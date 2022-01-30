42251d736a6bc14f7905ec038ee605c4838fa13f
serhioromano <serhioromano@outlook.com>
2022-01-30 20:47:36 +0600

    docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,35 @@
 
 set -e
 
-echo "::exho::on"
-
-if [ $1 == "yes" ];
-then
-    echo "::notice ::Yes works"
+if [ "$1" = "yes" ]; then
+    # vsce package
+    toush test.txt
+    filelist=(`ls *.txt`)
+    file=`realpath "$PWD/${filelist[*]}"`
+    echo "::notice ::Generating a VSIX file at $file"
+    echo "::set-output name=vsix_path::$file"
 fi
+# vsce publish -p $2
 
-echo "::notice ::Access to env $GITHUB_ACTION"
-sh -c "echo '::notice ::Access to env $GITHUB_REPOSITORY'"
-echo "::notice ::Access to local env $MYDATA"
-echo "Hello $1"
-echo "Hello $2"
-time=$(date)
-echo "::set-output name=output1::$time"
-touch test.txt
-echo "Some staff to file" > test.txt
+
+
+#!/bin/sh -l
+
+# set -e
+
+# echo "::exho::on"
+
+# if [ $1 == "yes" ];
+# then
+#     echo "::notice ::Yes works"
+# fi
+
+# echo "::notice ::Access to env $GITHUB_ACTION"
+# sh -c "echo '::notice ::Access to env $GITHUB_REPOSITORY'"
+# echo "::notice ::Access to local env $MYDATA"
+# echo "Hello $1"
+# echo "Hello $2"
+# time=$(date)
+# echo "::set-output name=output1::$time"
+# touch test.txt
+# echo "Some staff to file" > test.txt
