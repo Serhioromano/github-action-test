efff46d6b37a15e9cbb831c2bfdd6d0a7164e9cf
serhioromano <serhioromano@outlook.com>
2022-01-30 21:07:43 +0600

    w

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ if [ $1 == "yes" ]; then
     touch test.txt
     echo "123" > test.txt
     filelist=$(ls *.txt)
-    file=$(realpath "${PWD}/${filelist}")
+    file=$(realpath "${PWD}/${filelist[0]}")
     echo "::notice ::Generating a VSIX file at ${file}"
     echo "::set-output name=vsix_path::${file}"
 fi
