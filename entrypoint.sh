1cec5fa9d13477d5f708d28b57d2a4a3d9e4218f
serhioromano <serhioromano@outlook.com>
2022-01-30 20:53:31 +0600

    qw

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 set -e
 
 if [ $1 == "yes" ]; then
-    # vsce package
     touch test.txt
     echo "123" > test.txt
     filelist=`ls *.txt`
@@ -11,27 +10,3 @@ if [ $1 == "yes" ]; then
     echo "::notice ::Generating a VSIX file at $file"
     echo "::set-output name=vsix_path::$file"
 fi
-# vsce publish -p $2
-
-
-
-#!/bin/sh -l
-
-# set -e
-
-# echo "::exho::on"
-
-# if [ $1 == "yes" ];
-# then
-#     echo "::notice ::Yes works"
-# fi
-
-# echo "::notice ::Access to env $GITHUB_ACTION"
-# sh -c "echo '::notice ::Access to env $GITHUB_REPOSITORY'"
-# echo "::notice ::Access to local env $MYDATA"
-# echo "Hello $1"
-# echo "Hello $2"
-# time=$(date)
-# echo "::set-output name=output1::$time"
-# touch test.txt
-# echo "Some staff to file" > test.txt
