8e8949652fa3a027545e2c843c38b40e5a631ef3
serhioromano <serhioromano@outlook.com>
2022-01-30 21:47:51 +0600

    test dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,8 @@ if [ $1 == "yes" ]; then
     touch test.txt
     echo "123" > test.txt
     filelist=(`ls *.txt`)
-    file=$(realpath "${PWD}/${filelist[*]}")
+    file=$(realpath "${PWD}a/${filelist[*]}")
+    echo $file | grep "No such file"
     echo "::notice ::Generating a VSIX file at ${file}"
     echo "::set-output name=vsix_path::${file}"
     ls -la
