6bf5ce5c22755df2418268155b1d2171b202716c
serhioromano <serhioromano@outlook.com>
2022-01-30 20:49:00 +0600

    docker2

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-if [ "$1" = "yes" ]; then
+if [ $1 == "yes" ]; then
     # vsce package
     toush test.txt
-    filelist=(`ls *.txt`)
+    filelist=`ls *.txt`
     file=`realpath "$PWD/${filelist[*]}"`
     echo "::notice ::Generating a VSIX file at $file"
     echo "::set-output name=vsix_path::$file"
